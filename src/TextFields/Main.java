618f08bbf511124d245ba61045dfb7a37d2a1c64
dogukangulturk <dogukangulturk@gmail.com>
2022-09-10 02:12:07 +0300

    TextField added!

---
+++ src/TextFields/Main.java
@@ -0,0 +1,11 @@
+package TextFields;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //! JTextFields = A GUI text box component that can be used to add, set or get text
+
+        MyFrame frame = new MyFrame();
+
+    }
+}
